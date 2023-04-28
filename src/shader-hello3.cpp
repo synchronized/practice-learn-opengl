20e006004edbfd0c5adb862d0557fdd4f3b79188
sunday <synchronized@yeah.net>
2023-04-28 16:01:30 +0800

    修复windows下shader-hello3编译错误的问题

--- src/shader-hello3.cpp
+++ src/shader-hello3.cpp
@@ -112,7 +112,6 @@ void Shader::close() {
 //-----------------------------------------------
 class Line {
   public:
-    std::array<float, 6> vertices;
     unsigned int VBO, VAO;
 
     Line(float p1x, float p1y, float p2x, float p2y);
@@ -122,14 +121,14 @@ class Line {
 };
 
 Line::Line(float p1x, float p1y, float p2x, float p2y) {
-    vertices = {p1x, p1y, 0.0f, p2x, p2y, 0.0f};
+    float vertices[] = {p1x, p1y, 0.0f, p2x, p2y, 0.0f};
 
     glGenBuffers(1, &VBO);
     glGenVertexArrays(1, &VAO);
 
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, vertices.size()*sizeof(float), vertices.cbegin(), GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 }
