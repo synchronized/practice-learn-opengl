62ec54b359914e99f5d459eaf44934b2d7a822a9
sunday <synchronized@yeah.net>
2023-04-28 15:59:32 +0800

    修复纹理白边的问题

--- src/texture-hello.cpp
+++ src/texture-hello.cpp
@@ -138,9 +138,10 @@ int Texture::open(const std::string &texPath, unsigned int format) {
     // 为当前绑定的纹理对象设置环绕、过滤方式
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
+    stbi_set_flip_vertically_on_load(true);
     int width, height, nrChannels;
     unsigned char *data = stbi_load(texPath.c_str(), &width, &height, &nrChannels, 0);
     if (data == NULL) {
